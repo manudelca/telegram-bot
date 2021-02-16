bd4ba5f0780c8551f024f5bbdb073cf7c9d34f4f
Santiago Alvarez Juliá <santiago.alvarezjulia@gmail.com>
2021-02-16 15:55:25 -0300

    [Version] 0.2.1 - fix rubocop

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -1,3 +1,3 @@
 def movie_details_formatted(content)
-  "#{content['name']}, #{content['audience']}, #{content['duration_minutes']} minutes, #{content['genre']}, #{content['country']}, #{content['director']}, #{content['first_actor']}, #{content['second_actor']}"
+  "#{content['name']}, #{content['audience']}, #{content['duration_minutes']} minutes, #{content['genre']}, #{content['country']}, #{content['director']}, #{content['first_actor']}, #{content['second_actor']}" # rubocop:disable Metrics/LineLength
 end
